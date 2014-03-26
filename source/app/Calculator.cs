e5dc1a5e2a00f973dcaafdd4febe293cca587eb5
kanwu <wukan@hotmail.com>
2014-03-26 10:04:00 -0600

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -1,9 +1,14 @@
-﻿namespace app
+﻿using System;
+using System.IO;
+
+namespace app
 {
   public class Calculator
   {
     public int add(int first, int second)
     {
+        if (first < 0 || second < 0) throw new ArgumentException();
+
         return first + second;
     }
   }
